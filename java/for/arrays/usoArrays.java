2f2ec52ebbc3d3ff07aca5adbaf4a37890646e61
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-05 09:34:43 +0100

    Add usoArrays.java for country name input

---
+++ java/for/arrays/usoArrays.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		String[]  Paises = new String [5];
+
+		System.out.println("Introduce nombres de Paises\n");
+
+		for (int i = 0; i < Paises.length; i++) {
+
+			Paises[i] = input.nextLine();
+
+		}
+
+        System.out.println("Tu lista de paises\n");
+
+		for (String paises : Paises) System.out.println(paises);
+
+
+	}
+}
