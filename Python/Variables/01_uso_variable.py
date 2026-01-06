1464b835044b59ae103d536ad5026fc0b00da1f7
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-06 16:26:54 +0100

    Add variable declarations with type annotations
    Added variable declarations with comments explaining their types.

---
+++ Python/Variables/01_uso_variable.py
@@ -0,0 +1,11 @@
+# Las variables sirven para almacenar datos la RAM
+# Durante la ejecución 
+
+nombre = "Jonathan" #tipo String «str» significa cadena de caracteres
+
+edad = 19 #tipo integer «int» significa numero entero
+
+salario = 1200.0 #tipo float «float» significa numero con decimal
+
+trabajando = True #tipo boolean «bool» significa verdad
+estudiando = False #tipo boolean «bool» significa falso
