3ba0e604ad6c34226a51ea3fc47ba7f80475832c
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-10 00:46:22 +0100

    Create sintaxis_operadores_logicos.py

---
+++ Python/Operadores_logicos/sintaxis_operadores_logicos.py
@@ -0,0 +1,30 @@
+# operadores lógicos devuelven un True o un False
+
+# operador «and» significa «y»
+# el and solo devuelve el True si todas las condiciones son verdaderas
+print(True and True) # las dos condiciones son verdaderas
+# salida: True 
+
+# si solo una condición se cumple la condición es falsa
+print(True and False) # la condición es verdadera y falsa
+# salida: False
+
+# si las dos condiciones son falsas la condición es Falsa
+print(False and False) # la condición es falso y falso
+# salida: False
+
+
+# operador «or» significa «o»
+# devuelve True si solo una condición es verdadera
+print(True or True) # la condición es verdadero o verdadero
+# salida True
+
+# si uno es verdadero la condición es verdadero
+print(True or False) # la condición es verdadero o falso
+# salida: True
+
+# si las dos condiciones son falsas la condición es falsa
+print(False or False) # la condición es falso o falso
+# salida: False
+
+# próximamente el not
