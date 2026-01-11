cb276b76e58cb01de0460de161211db850f5f018
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-11 02:54:02 +0100

    Update uso_def.py

--- Python/Def/uso_def.py
+++ Python/Def/uso_def.py
@@ -1,10 +1,9 @@
-# entrada al input
-nombre = input("escribe tu nombre: ").lower().strip()
+# entrada
+nombre = input("escribe tu nombre: ").lower().strip().replace(" ", "") # reemplaza un caracter por otro
 
 # la funcion devuelve si es o no es un palindromo
 def es_palindromo(texto): # parametro
 
-    # la condicion que compara
     if texto == texto[::-1]:
         print("Es un palindromo")
     else:
