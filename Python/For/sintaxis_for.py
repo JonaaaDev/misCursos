ef17fa42785a173a125750b66b93c2d453dce16e
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-12 16:15:39 +0100

    Create sintaxis_for.py

---
+++ Python/For/sintaxis_for.py
@@ -0,0 +1,14 @@
+# for es un bucle que se usa para recorrer iterables
+
+# creamos una variable llamada i que es la primera letra de «iterar»
+# la variable se puede llamar como quieras
+for i in "Jonathan": # iteramos la palabra «Palabra»
+    print(i) # imprime i
+
+# ***se puede iterar***
+#    strings
+#    listas
+#    tuplas
+#    diccionarios
+#    set
+#    rangos
