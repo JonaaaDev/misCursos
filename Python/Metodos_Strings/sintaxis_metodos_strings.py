f4f49d42980ba67d6438918a1f7702cf9b006797
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-13 00:12:12 +0100

    Create sintaxis_metodos_strings.py

---
+++ Python/Metodos_Strings/sintaxis_metodos_strings.py
@@ -0,0 +1,30 @@
+formatos = "estoy aprendiendo python  " # variable
+
+
+# con upper devuelves los carácteres en mayusculas
+print(formatos.upper()) 
+# salida: "ESTOY APRENDIENDO PYTHON  "
+
+# con lower devuelves los carácteres en minúsculas 
+print(formatos.lower())
+# salida: "estoy aprendiendo python "
+
+# con capitalize haces que la primera letra esté en mayuscula y lo demás en minúscula 
+print(formatos.capitalize())
+# salida: "Estoy aprendiendo python  "
+
+# con title devuelves los primeros caracteres de una palabra en mayuscula
+print(formatos.title())
+# salida: "Estoy Aprendiendo Python  "
+
+# con strip eliminas los espacios de la derecha y de la izquierda de una frase
+print(formatos.strip())
+# salida: "estoy aprendiendo python"
+
+# con lstrip eliminas los espacios de la derecha
+print(formatos.lstrip())
+#salida: "estoy aprendiendo python"
+
+# con rstrip eliminas los espacios de la izquierda
+print(formatos.rstrip())
+# salida: "estoy aprendiendo python"
