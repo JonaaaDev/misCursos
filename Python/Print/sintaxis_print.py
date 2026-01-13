c7926ba8a8454cd0f3fd01336e5743ebd008899c
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-13 23:01:06 +0100

    Create sintaxis_print.py

---
+++ Python/Print/sintaxis_print.py
@@ -0,0 +1,13 @@
+# print sintaxis
+
+# esto seria la función print, sirve para mostrar resultados en la terminal
+# es muy útil para ver si el programa funciona correctamente o si tenemos Python instalado correctamente 
+
+print() 
+
+# dentro de los paréntesis pondremos lo que queremos mostrar.
+# podemos imprimir texto, variables, listas... y mas
+
+
+
+
