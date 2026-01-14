f3930b59d3e188be7f31c3c777546cfc73e51952
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-14 01:58:48 +0100

    Create variables_integer.py

---
+++ Python/Fundamentos-Basicos/Variables/variables_integer.py
@@ -0,0 +1,18 @@
+# guardar integers (números enteros)
+
+# las variables de tipo «int» guardan números sin decimales
+# sirven para llevar cuentas, niveles, edades o cualquier dato matemático
+
+# guardamos la cantidad de vidas iniciales
+vidas = 3
+
+# guardamos la puntuación del jugador
+puntuacion = 0
+
+# guardamos el nivel actual
+nivel = 1
+
+# ¡Recordad! que los numeros enteros (integer) no van entre comillas
+# entre comillas van los strings texto. es importante entender esto 
+# si en algun momento queremos sumar restar o cualquer cosa con el numero
+# no podremos hacerlo si es un string
