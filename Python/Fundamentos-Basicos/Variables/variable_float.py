c3ba5188c6498e17dace8431dcb86ec42f012fa6
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-14 02:03:51 +0100

    Create variable_float.py

---
+++ Python/Fundamentos-Basicos/Variables/variable_float.py
@@ -0,0 +1,16 @@
+# guardar floats (números decimales)
+
+# las variables de tipo «float» guardan números con parte decimal
+# se usan para precios, estaturas, pesos o promedios
+
+# guardamos el precio de un producto
+precio_pan = 1.20
+
+# guardamos la altura de una persona (en metros)
+altura = 1.75
+
+# guardamos el valor de pi
+pi = 3.1416
+
+# ¡Recordad!: para los decimales se usa el punto «.» 
+# Si usas la coma «,» el programa pensará que son dos números distintos
