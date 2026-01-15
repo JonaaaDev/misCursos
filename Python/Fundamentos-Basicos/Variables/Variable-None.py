ff7358120f5274416f46c9dbe1086ca1b8d43674
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-15 15:52:25 +0100

    Create Variable-None.py

---
+++ Python/Fundamentos-Basicos/Variables/Variable-None.py
@@ -0,0 +1,16 @@
+# antes de empezar aclarar que None no es lo mismo que cero
+
+
+# el 0 es un número entero (un valor matemático)
+# el None es la ausencia total de valor (el vacío)
+
+# puntos = 0 el usuario tiene cero puntos (ya empezó a jugar)
+
+puntos = None # el usuario ni siquiera tiene puntaje (no ha jugado)
+
+# si guardas None, python entiende que la variable está «esperando» un dato
+
+# ***diferencias clave***
+#    0 es un valor de tipo «int» (entero)
+#    None es un valor de tipo «nonetype» (vacío)
+#    en un examen, un 0 es una nota, un None es no haberse presentado
